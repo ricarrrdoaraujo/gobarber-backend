52717f8fc525c67c47732bfdf7c1a3216b4e6a12
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-01-16 00:59:13 -0300

    Gerando token JWT

--- src/service/AuthenticateUserService.ts
+++ src/service/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -33,8 +35,14 @@ class AuthenticateUserService {
 
     // Usuário autenticado
 
+    const token = sign({}, 'a7866fb65d2ad45bc4c901524d2babe8', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
